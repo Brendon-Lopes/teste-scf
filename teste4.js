7e06c89fe66b6cdd544f4eb357b156ec1099f767
Brendon Lopes <brendon.lopes.21@gmail.com>
2023-06-14 22:35:12 -0300

    teste 4 completo

--- teste4.js
+++ teste4.js
@@ -1,13 +1,17 @@
-var data =  require("./fakeData");
+const data = require("./fakeData");
 
-module.exports =  function(req, res) {
-  
-    var id =  req.query.id;
+module.exports = function (req, res) {
+    const { id } = req.query
+    const { name, job } = req.body;
 
-    const reg = data.find(d => id == id);
-    reg.name = req.body.name;
-    reg.job = req.body.job;
+    const user = data.find(user => user.id === Number(id));
 
-    res.send(reg);
+    if (user) {
+        user.name = name;
+        user.job = job;
 
+        res.status(200).send(user);
+    } else {
+        res.status(404).send({ error: "User not found" });
+    }
 };
