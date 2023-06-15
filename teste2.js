a637718ab6ae8d648430aafda09d5ffab18a9228
Brendon Lopes <brendon.lopes.21@gmail.com>
2023-06-14 22:38:36 -0300

    fix: melhora a criação de id para usuarios novos

--- teste2.js
+++ teste2.js
@@ -3,8 +3,10 @@ const data = require("./fakeData");
 module.exports = function (req, res) {
     const { name, job } = req.body;
 
+    const lastId = data[data.length - 1].id;
+
     const newUser = {
-        id: data.length + 1, // simulando o auto increment de um banco de dados.
+        id: lastId + 1, // simulando o auto increment de um banco de dados.
         name,
         job,
     };
