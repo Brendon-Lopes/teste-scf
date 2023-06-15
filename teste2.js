26218bb83fef70c8e1cefc899aa0ded45c6e9950
Brendon Lopes <brendon.lopes.21@gmail.com>
2023-06-14 22:13:50 -0300

    teste 2 completo

--- teste2.js
+++ teste2.js
@@ -1,17 +1,14 @@
-var data =  require("./fakeData");
+const data = require("./fakeData");
 
-module.exports = function(req, res){
-  
-    var name =  req.body.name;
-    var jov =  req.body.job;
-    
-    var newUser = {
-        name: name,
-        job: job,
-    }
+module.exports = function (req, res) {
+    const { name, job } = req.body;
 
-    data.push(newUser)
-    
-    res.send(newUser);
+    const newUser = {
+        id: data.length + 1, // simulando o auto increment de um banco de dados.
+        name,
+        job,
+    };
 
+    data.push(newUser);
+    res.status(201).json(newUser);
 };
